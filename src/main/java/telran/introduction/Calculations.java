2be36274d3040e60306ca85e6dbbdfc841df44d1
Daniel Sender <70211437+daniel1sender@users.noreply.github.com>
2024-07-06 21:52:25 +0300

    add calculations classes

---
+++ src/main/java/telran/introduction/Calculations.java
@@ -0,0 +1,65 @@
+package telran.introduction;
+
+public class Calculations {
+    public static int sum(int op1, int op2) {
+        int res = op1 + op2;
+        return res;
+    }
+
+    public static int multiply(int op1, int op2) {
+        int res = op1 * op2;
+        return res;
+    }
+
+    public static int divide(int op1, int op2) {
+        if (op2 == 0) {
+            throw new IllegalArgumentException("Division by zero is not allowed.");
+        }
+        int res = op1 / op2;
+        return res;
+    }
+
+    public static int subtract(int op1, int op2) {
+        int res = op1 - op2;
+        return res;
+    }
+
+    public static int sumOfDigits(int number) {
+        int res = 0;
+        if (number < 0) {
+            number = -number;
+        }
+        while (number > 0) {
+            int digit = number % 10;
+            number /= 10;
+            res += digit;
+        }
+        return res;
+    }
+
+    public static int maxDigit(int number) {
+        int max = 0;
+        if (number < 0) {
+            number = -number;
+        }
+        while (number > 0) {
+            int digit = number % 10;
+            if (digit > max) {
+                max = digit;
+            }
+            number /= 10;
+        }
+        return max;
+    }
+
+    public static boolean isDividedOn(int number, int dividor) {
+        if (dividor == 0) {
+            throw new IllegalArgumentException("Division by zero is not allowed.");
+        }
+        if (number % dividor == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
---
+++ src/test/java/telran/introduction/CalculationTest.java
@@ -0,0 +1,44 @@
+package telran.introduction;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import static telran.introduction.Calculations.*;
+
+public class CalculationTest {
+    @Test
+    void sumTest() {
+        assertEquals(4, sum(2, 2));
+        assertEquals(-2, sum(-1, -1));
+        assertEquals(0, sum(0, 0));
+    }
+    @Test
+    void multiplyTest() {
+        assertEquals(4, multiply(2, 2));
+    }
+    @Test
+    void divideTest() {
+        assertEquals(2, divide(4, 2));
+        assertThrows(IllegalArgumentException.class, () -> divide(4, 0));
+    }
+    @Test
+    void subtractTest() {
+        assertEquals(1, subtract(2, 1));
+    }
+    @Test
+    void sumOfDigitsTest() {
+        assertEquals(6, sumOfDigits(123));
+        assertEquals(6, sumOfDigits(-123));
+    }
+    @Test
+    void maxDigitsTest() {
+        assertEquals(3, maxDigit(123));
+        assertEquals(5, maxDigit(145032));
+        assertEquals(5, maxDigit(-145023));
+    }
+    @Test
+    void isDividedOnTest() {
+        assertTrue(isDividedOn(12, 3));
+        assertFalse(isDividedOn(-4, -3));
+        assertThrows(IllegalArgumentException.class, () -> divide(4, 0));
+    }
+}
